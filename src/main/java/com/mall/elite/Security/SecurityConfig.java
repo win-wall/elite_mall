fc5d289df95300758a0e803f9e74a4605b61e832
Win Wall <76393935+win-wall@users.noreply.github.com>
2022-04-19 11:03:15 +0700

    Create JWTtokenprovider and Flitter

--- src/main/java/com/mall/elite/Security/SecurityConfig.java
+++ src/main/java/com/mall/elite/Security/SecurityConfig.java
@@ -1,8 +1,14 @@
 package com.mall.elite.Security;
 
+import com.mall.elite.Security.jwt.JwtAuthenticationFilter;
+import com.mall.elite.Service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.BeanIds;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -11,16 +17,36 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    UserDetailService userDetailService;
+
     @Bean
     public PasswordEncoder passwordEncoder(){
         //! Spring Security will help us to encode the password
         return new BCryptPasswordEncoder();
     }
+    @Bean
+    public JwtAuthenticationFilter jwtAuthenticationFilter(){
+        return new JwtAuthenticationFilter();
+    }
+    @Bean(BeanIds.AUTHENTICATION_MANAGER)
+    @Override
+    public AuthenticationManager authenticationManager() throws Exception{
+        // Get AuthenticationManager bean
+        return super.authenticationManagerBean();
+    }
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+        auth.userDetailsService(userDetailService) //! This line is we provide UserDetail Service cho Spring Security
+                .passwordEncoder(passwordEncoder());
+    }
+
     @Bean
     @Override
     //* this "UserDetailsService provide Spring security a user
@@ -48,5 +74,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .logout()
                 .permitAll();
+        http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
     }
 }
---
+++ src/main/java/com/mall/elite/Security/jwt/JwtTokenProvider.java
@@ -0,0 +1,52 @@
+package com.mall.elite.Security.jwt;
+
+import com.mall.elite.Security.UserDetail;
+import io.jsonwebtoken.*;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Slf4j
+@Component
+public class JwtTokenProvider {
+    //! This is the secret key for JWT
+    private final String JWT_SECRET = "elite";
+    //!Time for a key to expire
+    private final long JWT_EXPIRATION = 604800000L;
+
+    private String generateTokenFromUser(UserDetail userDetail){
+        Date date = new Date();
+        Date expiryDate =new Date(date.getTime()+JWT_EXPIRATION);
+        return Jwts.builder()
+                .setSubject(Long.toString(userDetail.getUser().getId()))
+                .setIssuedAt(date)
+                .setExpiration(expiryDate)
+                .signWith(SignatureAlgorithm.HS512, JWT_SECRET)
+                .compact();
+    }
+    public String getUsernameFromJWT(String token){
+        Claims claims = Jwts.parser()
+                .setSigningKey(JWT_SECRET)
+                .parseClaimsJws(token)
+                .getBody();
+        return claims.getSubject();
+    }
+    public boolean validateToken(String token){
+        try{
+            Jwts.parser().setSigningKey(JWT_SECRET).parseClaimsJws(token);
+            return true;
+        } catch (MalformedJwtException ex) {
+            log.error("Invalid JWT token");
+        } catch (ExpiredJwtException ex) {
+            log.error("Expired JWT token");
+        } catch (UnsupportedJwtException ex) {
+            log.error("Unsupported JWT token");
+        } catch (IllegalArgumentException ex) {
+            log.error("JWT claims string is empty.");
+        }
+
+        return false;
+    }
+}
+
